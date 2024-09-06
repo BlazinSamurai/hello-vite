9fc8929010e59628cdb1ab60b1d442c220db9142
DamianLavin <damianmlavin@gmail.com>
2024-09-05 18:43:49 -0700

    some refactoring

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import Header from "./Header";
+import Main from "./Main";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
